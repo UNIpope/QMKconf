fddd8170a1000a6f34ba9153ec6f8dd03524b87a
UNIpope <UNIpope@users.noreply.github.com>
2023-08-01 11:28:56 +0200

    Add files via upload

---
+++ config.h
@@ -0,0 +1,15 @@
+// Obins stock firmware has something similar to this already enabled, but disabled by default in QMK
+#define PERMISSIVE_HOLD
+
+#define RGB_MATRIX_STARTUP_VAL 2
+#define RGB_MATRIX_DEFAULT_VAL 2
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 5
+
+#define RGBLIGHT_LIMIT_VAL 5
+
+#define _LAYER_BASE_COLOR RGB_BLUE
+#define _LAYER_1_COLOR 0x89, 0x6C, 0xBC
+#define _INDICATOR_COLOR RGB_RED
+
+#define LED_CAPS 28
+#define LED_LSHIFT 41
---
+++ keymap.c
@@ -0,0 +1,144 @@
+#include <stdint.h>
+#include "annepro2.h"
+#include "qmk_ap2_led.h"
+#include "config.h"
+
+enum anne_pro_layers {
+  _BASE_LAYER,
+  _FN1_LAYER,
+};
+
+/*
+* Layer _BASE_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* | esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |    Bksp   |
+* |-----------------------------------------------------------------------------------------+
+* | Tab    |  q  |  w  |  e  |  r  |  t  |  y  |  u  |  i  |  o  |  p  |  [  |  ]  |  \|    |
+* |-----------------------------------------------------------------------------------------+
+* | Caps    |  a  |  s  |  d  |  f  |  g  |  h  |  j  |  k  |  l  |  ;  |  '  |    Enter    |
+* |-----------------------------------------------------------------------------------------+
+* | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |  .  |  /  |    Shift       |
+* |-----------------------------------------------------------------------------------------+
+* | Ctrl  |  FN1  |  Alt  |               space             |  Alt  |  GUI  |  ---  | Ctrl  |
+* \-----------------------------------------------------------------------------------------/
+* Layer TAP in _BASE_LAYER
+* ,-----------------------------------------------------------------------------------------.
+* |     |     |     |     |     |     |     |     |     |     |     |     |     |           |
+* |-----------------------------------------------------------------------------------------+
+* |        |     |     |     |     |     |     |     |     |     |     |     |     |        |
+* |-----------------------------------------------------------------------------------------+
+* |         |     |     |     |     |     |     |     |     |     |     |     |             |
+* |-----------------------------------------------------------------------------------------+
+* |            |     |     |     |     |     |     |     |     |     |     |       UP       |
+* |-----------------------------------------------------------------------------------------+
+* |       |       |       |                                 |       |       |        | DOWN |
+* \-----------------------------------------------------------------------------------------/
+
+*/
+ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ [_BASE_LAYER] = KEYMAP( /* Base */
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP),
+    KC_LCTL, MO(_FN1_LAYER), KC_LALT, KC_SPC, KC_RALT, KC_LGUI, KC_F14, RCTL_T(KC_DOWN)
+),
+  /*
+  * Layer _FN1_LAYER
+  * ,-----------------------------------------------------------------------------------------.
+  * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
+  * |-----------------------------------------------------------------------------------------+
+  * | Tab    | F13 | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+  * |-----------------------------------------------------------------------------------------+
+  * | Shift      |V-UP |V-DWN|MUTE |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
+  * |-----------------------------------------------------------------------------------------+
+  * | Ctrl  |  L1   |  Alt  |               space             |  Alt  |  FN1  |  ---  | Ctrl  |
+  * \-----------------------------------------------------------------------------------------/
+  *
+  */
+ [_FN1_LAYER] = KEYMAP( /* Base */
+    KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+    KC_TRNS, KC_F13, KC_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_HOME, KC_END, KC_TRNS,
+    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_PGDN, KC_TRNS,
+    KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_DEL, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+),
+};
+const uint16_t keymaps_size = sizeof(keymaps);
+
+
+void matrix_init_user(void) {
+
+}
+
+void matrix_scan_user(void) {
+}
+
+// Code to run after initializing the keyboard
+void keyboard_post_init_user(void) {
+    // Enable RGB from the start
+    annepro2LedEnable();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+
+    annepro2LedNextIntensity();
+    annepro2LedResetForegroundColor();
+   
+}
+
+layer_state_t layer_state_set_user(layer_state_t layer) {
+    return layer;
+}
+
+
+/**
+ * Called after RBG effect render.
+ */
+void rgb_matrix_indicators_user() {
+   
+    switch(get_highest_layer(layer_state|default_layer_state)) { 
+        case _FN1_LAYER:
+            rgb_matrix_set_color_all(_LAYER_1_COLOR); 
+            break;
+        case _BASE_LAYER:
+            rgb_matrix_set_color_all(_LAYER_BASE_COLOR); 
+            break;
+        default:
+            break;
+    }
+
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_set_color(LED_CAPS, _INDICATOR_COLOR);
+        rgb_matrix_set_color(LED_LSHIFT, _INDICATOR_COLOR); 
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_F13:
+            if (record->event.pressed) {
+                SEND_STRING("jack.dugg.ch@gmail.com");
+            }
+            return false;
+    }
+    return true;
+}
+
